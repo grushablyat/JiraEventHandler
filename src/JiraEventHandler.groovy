0ea3f149e797b12bb7e10083c6408f5654a9d9e4
hector <tselousovgrigorij@gmail.com>
2024-08-04 14:47:35 +0300

    Update JiraEventHandler.groovy

--- src/JiraEventHandler.groovy
+++ src/JiraEventHandler.groovy
@@ -1,62 +1,13 @@
 import com.atlassian.jira.issue.Issue
-import com.atlassian.jira.component.ComponentAccessor
 
 String url
 Map<String, Object> params = new LinkedHashMap<>()
 
-def issue=event.getIssue()
+String ikey = event.issue.getKey()
+params.put("ikey", ikey)
 
-def changeItems = ComponentAccessor.getChangeHistoryManager().getAllChangeItems(issue)
-def length = changeItems.size()
-
-if(changeItems[length-1].getProperties()["field"] == "status"){
-    String ikey = event.issue.getKey()
-    params.put("ikey", ikey)
-
-    // String status = convertToString(changeItems[length-1].getTos())
-    // params.put("status", status)
-
-    // params.put("status", changeItems[length-1].getProperties().status)
-
-    url = "https://renewing-dinosaur-generally.ngrok-free.app/transition_made"
-    String result = executePost(url, params)
-}
-
-// // def change = event?.getChangeLog()?.getRelated("ChildChangeItem")?.find { it.field == "status" }
-
-// // Issue issue = event.getIssue()
-// String ikey = event.issue.getKey()
-// // String tostring = event.toString()
-// // long event_type_id = event.getEventTypeId()
-
-// params.put("ikey", ikey)
-// // params.put("status" actionName)
-// // params.put("tostring", event.toString())
-// // params.put("status", issue.getStatus().name)
-// // params.put("reporter", issue.reporter.name)
-
-// // StatusEditedEvent statusEditedEvent = (StatusEditedEvent)event
-
-// // String status = statusEditedEvent.getOriginalStatus().name
-
-// // params.put("status", status)
-
-// url = "https://renewing-dinosaur-generally.ngrok-free.app/transition_made"
-
-// String result = executePost(url, params)
-
-static String convertToString(Map<?, ?> map) {
-    if (map.isEmpty) {
-        return "NONE"
-    }
-
-    StringBuilder mapAsString = new StringBuilder("{");
-    for (var key : map.keySet()) {
-        mapAsString.append("" + key + "=" + map.get(key) + ", ");
-    }
-    mapAsString.delete(mapAsString.length()-2, mapAsString.length()).append("}");
-    return mapAsString.toString();
-}
+url = Config.url + Config.action
+String result = executePost(url, params)
 
 static String executePost(String targetURL, Map<String, Object> params) {
 
